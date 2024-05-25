a17e655ec3f3213652d8cbe71f2658c7fbec7ac1
Samed Kütahyalı <smdkthyli@gmail.com>
2024-05-25 17:51:00 +0300

    add OrderOutboxPublishJob with quartz library

---
+++ Order.Outbox.Table.Publisher.Service/Jobs/OrderOutboxPublishJob.cs
@@ -0,0 +1,17 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Outbox.Table.Publisher.Service.Jobs
+{
+    public class OrderOutboxPublishJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
