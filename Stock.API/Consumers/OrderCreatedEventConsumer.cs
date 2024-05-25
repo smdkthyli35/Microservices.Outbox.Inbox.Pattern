e06f44f8996cf0b12d7a57a9f4d24d84ed8fe106
Samed Kütahyalı <smdkthyli@gmail.com>
2024-05-25 18:50:40 +0300

    add inbox pattern process for order created event consumer

--- Stock.API/Consumers/OrderCreatedEventConsumer.cs
+++ Stock.API/Consumers/OrderCreatedEventConsumer.cs
@@ -1,13 +1,33 @@
 ﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
 using Shared.Events;
+using Stock.API.Models.Contexts;
+using Stock.API.Models.Entities;
+using System.Text.Json;
 
 namespace Stock.API.Consumers
 {
-    public class OrderCreatedEventConsumer : IConsumer<OrderCreatedEvent>
+    public class OrderCreatedEventConsumer(StockDbContext stockDbContext) : IConsumer<OrderCreatedEvent>
     {
-        public Task Consume(ConsumeContext<OrderCreatedEvent> context)
+        public async Task Consume(ConsumeContext<OrderCreatedEvent> context)
         {
-            throw new NotImplementedException();
+            OrderInbox orderInbox = new()
+            {
+                Processed = false,
+                Payload = JsonSerializer.Serialize(context.Message)
+            };
+
+            await stockDbContext.OrderInboxes.AddAsync(orderInbox);
+            await stockDbContext.SaveChangesAsync();
+
+            List<OrderInbox> orderInboxes = await stockDbContext.OrderInboxes.Where(x => x.Processed == false).ToListAsync();
+            foreach (var ordInbox in orderInboxes)
+            {
+                OrderCreatedEvent? orderCreatedEvent = JsonSerializer.Deserialize<OrderCreatedEvent>(ordInbox.Payload);
+                await Console.Out.WriteLineAsync($"{orderCreatedEvent.OrderId} order id değerine sahip olan siparişin stok işlemleri tamamlandı.");
+                ordInbox.Processed = true;
+                await stockDbContext.SaveChangesAsync();
+            }
         }
     }
 }
