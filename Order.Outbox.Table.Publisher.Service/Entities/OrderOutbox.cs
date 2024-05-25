bfe5cd34bdca4c97194f36650fb1c04ca4ee08d3
Samed Kütahyalı <smdkthyli@gmail.com>
2024-05-25 18:15:11 +0300

    complete OrderOutboxPublishJob

---
+++ Order.Outbox.Table.Publisher.Service/Entities/OrderOutbox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Outbox.Table.Publisher.Service.Entities
+{
+    public class OrderOutbox
+    {
+        public int Id { get; set; }
+        public DateTime OccuredOn { get; set; }
+        public DateTime? ProcessedDate { get; set; }
+        public string Type { get; set; }
+        public string Payload { get; set; }
+    }
+}
--- Order.Outbox.Table.Publisher.Service/Jobs/OrderOutboxPublishJob.cs
+++ Order.Outbox.Table.Publisher.Service/Jobs/OrderOutboxPublishJob.cs
@@ -1,17 +1,45 @@
-﻿using Quartz;
+﻿using MassTransit;
+using Order.Outbox.Table.Publisher.Service.Entities;
+using Quartz;
+using Shared.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Order.Outbox.Table.Publisher.Service.Jobs
 {
-    public class OrderOutboxPublishJob : IJob
+    public class OrderOutboxPublishJob(IPublishEndpoint publishEndpoint) : IJob
     {
-        public Task Execute(IJobExecutionContext context)
+        public async Task Execute(IJobExecutionContext context)
         {
-            throw new NotImplementedException();
+            if (OrderOutboxSingletonDatabase.DataReaderState)
+            {
+                OrderOutboxSingletonDatabase.DataReaderBusy();
+
+                List<OrderOutbox> orderOutboxes = (await OrderOutboxSingletonDatabase.QueryAsync<OrderOutbox>(
+                    $@"SELECT * FROM ORDEROUTBOXES WHERE PROCESSEDDATE IS NULL ORDER BY OCCUREDON ASC"))
+                    .ToList();
+
+                foreach (var orderOutbox in orderOutboxes)
+                {
+                    if (orderOutbox.Type == nameof(OrderCreatedEvent))
+                    {
+                        OrderCreatedEvent orderCreatedEvent = JsonSerializer.Deserialize<OrderCreatedEvent>(orderOutbox.Payload);
+                        if (orderCreatedEvent is not null)
+                        {
+                            await publishEndpoint.Publish(orderCreatedEvent);
+                            await OrderOutboxSingletonDatabase.ExecuteAsync(
+                                $"UPDATE ORDEROUTBOXES SET PROCESSEDDATE = GETDATE() WHERE ID = '{orderOutbox.Id}'");
+                        }
+                    }
+                }
+
+                OrderOutboxSingletonDatabase.DataReaderReady();
+                await Console.Out.WriteLineAsync("Order outbox table checked!");
+            }
         }
     }
 }
